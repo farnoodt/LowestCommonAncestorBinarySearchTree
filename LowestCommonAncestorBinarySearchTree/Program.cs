63be711110d977cfb1074ab1e9d61e736610d64a
Farnoud Taghdissi Borhani <Farnood_t@yahoo.com>
2021-08-09 20:45:48 -0700

    Add project files.

---
+++ LowestCommonAncestorBinarySearchTree/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LowestCommonAncestorBinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node tree = new Node(10);
+            tree.left = new Node(-10);
+            tree.left.right = new Node(8);
+            tree.left.right.left = new Node(6);
+            tree.left.right.right = new Node(9);
+            tree.right = new Node(30);
+            tree.right.left = new Node(25);
+            tree.right.left.right = new Node(28);
+            tree.right.right = new Node(60);
+            tree.right.right.right = new Node(78);
+
+            lowestCommonAncestor LCA = new lowestCommonAncestor();
+
+            Console.WriteLine(LCA.FindLowestCommonAncestor(tree, tree.right.right.right, tree.right.left ).data);
+        }
+    }
+}
---
+++ LowestCommonAncestorBinarySearchTree/lowestCommonAncestor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LowestCommonAncestorBinarySearchTree
+{
+    class lowestCommonAncestor
+    {
+        public Node FindLowestCommonAncestor(Node root, Node N1, Node N2)
+        {
+            
+            if (root.data > Math.Max(N1.data, N2.data))
+                return FindLowestCommonAncestor(root.left, N1, N2);
+            else if (root.data < Math.Min( N1.data ,N2.data ))
+                return FindLowestCommonAncestor(root.right, N1, N2);
+            else 
+                return root;
+
+        }
+    }
+}
